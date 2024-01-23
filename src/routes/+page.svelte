b6d7950dac88efe630a8431874c9d70c83585345
Kinlo Ephriam Tangiri (KET) <kinlotangiri911@gmail.com>
2024-01-23 01:31:53 +0100

    Setup project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>A project by Glen and Kinlo</title>
+    </head>
+    <body>
+        <h1>Welcome to Randezview</h1>
+        <h1>Hi there!</h1>
+        <p>This is a project by Glen and Kinlo.</p>
+        <p>It's a work in progress, but we're getting there!</p>
+        <p>Check out our <a href="https://kit.svelte.dev">kit.svelte.dev</a></p>
+    </body>
+</html>
